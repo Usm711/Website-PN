0998dc0eaae0c8260d1f61cd11c321b2e6f3a853
mubeenali-313 <mubeenali313996@gmail.com>
2025-08-15 00:10:16 +0500

    manage pages

--- src/components/BlogBackgroundImage.jsx
+++ src/components/BlogBackgroundImage.jsx
@@ -6,8 +6,8 @@ const BlogBackgroundImage = () => {
             <div className='image-background'>
         <img src="/images/blog.jpg" alt="portfolioImage" />
 
-        <div className='content'><h1>PORTFOLIO</h1>
-      <h3>Home <span>Portfolio</span></h3>
+        <div className='content'><h1>BLOG</h1>
+      <h3>Home <span>Blog</span></h3>
       </div>
     </div>
     </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,81 +1,67 @@
 import React from 'react';
 import '../Css/Contact.css';
-// import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaLinkedinIn, FaInstagram, FaFacebookF, FaTwitter, FaArrowRight } from 'react-icons/fa';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <div className="contact-section">
       <div className="contact-container">
+        <h2 className="section-title">Get In Touch</h2>
+        <p className="section-subtitle">LET'S TALK OUR TEAM WELL HELP YOU</p>
         
-        {/* Left Side: Contact Form */}
-        <div className="contact-form-wrapper">
-          <p className="get-in-touch">\\ Get In Touch \\</p>
-          <h2>LET'S TALK OUR TEAM WILL HELP YOU</h2>
-          
-          <form className="contact-form">
-            <div className="form-group">
-              <input type="text" name="name" placeholder="Name" required />
-            </div>
-            <div className="form-group">
-              <input type="email" name="email" placeholder="Email" required />
-            </div>
-            <div className="form-group">
-              <input type="tel" name="phone" placeholder="Phone" required />
-            </div>
-            <div className="form-group">
-              <textarea name="message" placeholder="Your Message" rows="6" required></textarea>
-            </div>
-            <button type="submit" className="send-button">
-              {/* Send Now <FaArrowRight className="arrow-icon" /> */}
-            </button>
-          </form>
-        </div>
-        
-        {/* Right Side: Contact Info */}
-        <div className="contact-info-wrapper">
-          <div className="info-item">
-            <div className="info-icon-bg">
-              {/* <FaPhoneAlt /> */}
-            </div>
-            <div className="info-text">
-              <h4>Call Anytime</h4>
-              <p>+92 348-1232271</p>
-              <p>+92 327-8593478</p>
-            </div>
+        <div className="contact-content">
+          {/* Left Column - Form */}
+          <div className="contact-form-wrapper">
+            <form className="contact-form">
+              <div className="form-group">
+                <input type="text" placeholder="Name" />
+              </div>
+              <div className="form-group">
+                <input type="text" placeholder="Phone" />
+              </div>
+              <div className="form-group">
+                <textarea placeholder="Your Message" rows="4"></textarea>
+              </div>
+              <button type="submit" className="submit-btn">Send Now</button>
+            </form>
           </div>
           
-          <div className="info-item">
-            <div className="info-icon-bg">
-              {/* <FaEnvelope /> */}
+          {/* Right Column - Info */}
+          <div className="contact-info-wrapper">
+            <div className="info-item">
+              <div className="info-icon">
+                <FaPhone />
+              </div>
+              <div className="info-content">
+                <h4>Call Anytime</h4>
+                <p>+ 52 345-622478</p>
+              </div>
             </div>
-            <div className="info-text">
-              <h4>Send Email</h4>
-              <p>info@youremail.com</p>
-              <p>pulseitpvtltd@gmail.com</p>
+            
+            <div className="info-item">
+              <div className="info-icon">
+                <FaEnvelope />
+              </div>
+              <div className="info-content">
+                <h4>Send Email</h4>
+                <p>phone@virtual@gmail.com</p>
+              </div>
             </div>
-          </div>
-          
-          <div className="info-item">
-            <div className="info-icon-bg">
-              {/* <FaMapMarkerAlt /> */}
-            </div>
-            <div className="info-text">
-              <h4>Visit Us</h4>
-              <p>Near Missile Chowk Main Mansehra road Mandian 1st floor, Pulseit (PVT) Limited IT Park Abbottabad, Pakistan</p>
-            </div>
-          </div>
-          
-          <div className="info-follow">
-            <h4>Follow us</h4>
-            <div className="social-icons">
-              {/* <a href="#" className="social-icon"><FaLinkedinIn /></a> */}
-              {/* <a href="#" className="social-icon"><FaInstagram /></a>
-              <a href="#" className="social-icon"><FaFacebookF /></a>
-              <a href="#" className="social-icon"><FaTwitter /></a> */}
+            
+            <div className="info-item">
+              <div className="info-icon">
+                <FaMapMarkerAlt />
+              </div>
+              <div className="info-content">
+                <h4>Visit Us</h4>
+                <p>New Media Chews Web</p>
+                <p>Newsletter chat website in</p>
+                <p>how Facebook (PC) photos rr</p>
+                <p>Post Aboutitized, Position</p>
+              </div>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   );
